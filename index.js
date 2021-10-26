4921a0bca90538da7d78830b779e87fe76335aee
kobe.lam <kobelam09@gmail.com>
2021-10-26 23:30:13 +0800

    feat(update): remove dependency on "stylelint-config-airbnb"

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 module.exports = {
-  extends: 'stylelint-config-airbnb',
+  plugins: [
+    'stylelint-scss',
+    'stylelint-order',
+  ],
   rules: {
+    // Base rules
     indentation: 2,
+    'number-leading-zero': 'never',
+    'string-quotes': 'single',
+    'selector-max-id': 1,
+    'selector-list-comma-newline-after': 'always',
+    'rule-empty-line-before': [
+      'always',
+      {
+        ignore: ['after-comment'],
+        except: ['first-nested'],
+      },
+    ],
+    'comment-empty-line-before': [
+      'always',
+      { except: ['first-nested'] },
+    ],
+    'block-opening-brace-space-before': 'always',
+    'declaration-colon-space-after': 'always',
+    'declaration-colon-space-before': 'never',
+    'declaration-block-single-line-max-declarations': 1,
+    'declaration-property-value-blacklist': { '/^border/': ['none'] },
     'at-rule-empty-line-before': [
       'always',
       {
@@ -10,23 +34,11 @@ module.exports = {
         except: ['first-nested', 'blockless-after-same-name-blockless'],
       },
     ],
+
+    // Sass rules
     'max-nesting-depth': null,
-    'rule-empty-line-before': [
-      'always',
-      {
-        ignore: ['after-comment'],
-        except: ['first-nested'],
-      },
-    ],
-    'selector-max-id': 1,
-    'string-quotes': 'single',
-    // stylelint-scss
-    'scss/at-else-empty-line-before': 'never',
-    'scss/at-mixin-argumentless-call-parentheses': 'always',
-    'scss/dollar-variable-colon-space-after': 'always',
-    'scss/selector-no-redundant-nesting-selector': true,
-    // stylelint-order
-    'order/properties-alphabetical-order': true,
+    'scss/dollar-variable-pattern': '^_?[a-z]+[\\w-]*$',
+    'scss/at-extend-no-missing-placeholder': true,
     'order/order': [
       { type: 'at-rule' },
       'declarations',
@@ -40,6 +52,14 @@ module.exports = {
         name: 'return',
       },
     ],
+
+    // stylelint-scss
+    'scss/at-else-empty-line-before': 'never',
+    'scss/at-mixin-argumentless-call-parentheses': 'always',
+    'scss/dollar-variable-colon-space-after': 'always',
+    'scss/selector-no-redundant-nesting-selector': true,
+    // stylelint-order
+    'order/properties-alphabetical-order': true,
   },
   defaultSeverity: 'error',
 };
